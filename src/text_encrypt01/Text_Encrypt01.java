e599ff65d5cfe4c75562c3217bccd487079b03d3
iszmor <iszmor@gmail.com>
2013-11-07 14:21:20 +0700

    new gui file

--- src/text_encrypt01/Text_Encrypt01.java
+++ src/text_encrypt01/Text_Encrypt01.java
@@ -1,15 +1,10 @@
-//Hello
+//Author: Piyapong pongteekayu, Suthipong Thongjaroen
+
+//This is the main class
 package text_encrypt01;
 
-/**
- *
- * @author briareos
- */
 public class Text_Encrypt01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }

d933be94def62d8e6e9ad0928f0d05d9b6d18db0
iszmor <iszmor@gmail.com>
2013-11-07 13:55:56 +0700

    new

---
+++ src/text_encrypt01/Text_Encrypt01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package text_encrypt01;
+
+/**
+ *
+ * @author briareos
+ */
+public class Text_Encrypt01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

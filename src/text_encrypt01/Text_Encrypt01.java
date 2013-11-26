8a19233e12858115ce8c167617b5ffb6a784c9ec
greanthai420 <suthipong.th@stiu-m-00013.local>
2013-11-26 14:47:11 +0700

    testing

--- src/text_encrypt01/Text_Encrypt01.java
+++ src/text_encrypt01/Text_Encrypt01.java
@@ -2,12 +2,21 @@
 
 //This is the main class
 package text_encrypt01;
+import java.util.Scanner;
 
 public class Text_Encrypt01 {
     
-    private static MainFrame frame;
+    //private static MainFrame frame;
     
     public static void main(String[] args) {
-        frame = new MainFrame();
+        //frame = new MainFrame();
+        Scanner kb = new Scanner(System.in);
+        System.out.println("word");
+        String word = kb.nextLine();
+        System.out.println("password");
+        String password = kb.nextLine();
+        Encrypt a = new Encrypt(word, password);
+        
+        //System.out.println("encrypted word is = " + a.returnWord());
     }
 }

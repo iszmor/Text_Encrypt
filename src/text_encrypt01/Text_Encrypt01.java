929dac89a3a894335aa7a331a2477f714d79ac8d
iszmor <iszmor@gmail.com>
2013-11-26 14:45:58 +0700

    remove unused code from Text_Encrypt01.java

--- src/text_encrypt01/Text_Encrypt01.java
+++ src/text_encrypt01/Text_Encrypt01.java
@@ -8,13 +8,5 @@ public class Text_Encrypt01 {
     private static MainFrame frame;
     
     public static void main(String[] args) {
-<<<<<<< HEAD
-        /*
-        //Create main windows
-        MainFrame frame = new MainFrame();
-                /*
-=======
-        frame = new MainFrame();
->>>>>>> afa272e6c71ddce92746e7d633e11effd1da4d45
     }
 }

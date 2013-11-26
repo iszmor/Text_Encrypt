6245247a70be592290998cf0e8354b8f17a86ae8
greanthai420 <suthipong.th@stiu-m-00013.local>
2013-11-26 14:49:15 +0700

    Merge branch 'master' of https://github.com/iszmor/Text_Encrypt
    Conflicts:
    	src/text_encrypt01/Text_Encrypt01.java

---
+++ src/text_encrypt01/EncryptDemo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package text_encrypt01;
+
+/**
+ *
+ * @author suthipong.th
+ */
+public class EncryptDemo {
+//    Scanner kb = new Scanner(System.in);
+//        System.out.println("word");
+//        String word = kb.nextLine();
+//        System.out.println("password");
+//        String password = kb.nextLine();
+//        Encrypt a = new Encrypt(word, password);
+//        
+//        //System.out.println("encrypted word is = " + a.returnWord());
+}
--- src/text_encrypt01/Text_Encrypt01.java
+++ src/text_encrypt01/Text_Encrypt01.java
@@ -2,21 +2,12 @@
 
 //This is the main class
 package text_encrypt01;
-import java.util.Scanner;
 
 public class Text_Encrypt01 {
     
     //private static MainFrame frame;
     
     public static void main(String[] args) {
-        //frame = new MainFrame();
-        Scanner kb = new Scanner(System.in);
-        System.out.println("word");
-        String word = kb.nextLine();
-        System.out.println("password");
-        String password = kb.nextLine();
-        Encrypt a = new Encrypt(word, password);
-        
-        //System.out.println("encrypted word is = " + a.returnWord());
+        //frame = new MainFrame();        
     }
 }

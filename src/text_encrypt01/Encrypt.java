ad516993bc7262e5b433eca73c837070399bf643
greanthai420 <suthipong.th@stiu-m-00013.local>
2013-11-28 15:33:00 +0700

    Revert "fug"
    This reverts commit 561f791430d2730ffac92673ee32d3e35ded9a7a.

--- src/text_encrypt01/Encrypt.java
+++ src/text_encrypt01/Encrypt.java
@@ -1,13 +1,20 @@
-/*
- Usage : call EncryptText(word, password) to encrypt @word with @password
- then call returnText to get the encrypted text
- */
 package text_encrypt01;
 
 public class Encrypt {
 
     private String word = "";
-    //private int algo = 0;
+    private int algo = 0;
+
+    public Encrypt() {
+//        //Test decrypt
+//        String test = "";
+//        for (int i = 0; i < word.length(); i++) {
+//            test += String.valueOf((char) (word.charAt(i) - algo));
+//            //System.out.println("char at i=" + i + " is " + (char) (word.charAt(i)));
+//        }
+//
+//        System.out.println("test is now scrambled and is now = " + test);
+    }
 
     public void EncryptText(String w, String pw) {
         /*
@@ -19,10 +26,6 @@ public class Encrypt {
             //Sum char code for all of the characters in password
             algotmp += pw.codePointAt(i);
         }
-        System.out.println("word length = " + w.length());
-        System.out.println("algotmp = " + algotmp);
-        System.out.println("algotmp length = " + String.valueOf(algotmp).length());
-
         /*
          * TODO: do loop to make algo be within the number of available character code
          * while also storing how many times it looped
@@ -30,6 +33,17 @@ public class Encrypt {
          * Valid codes : 33-126
          * http://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
          */
+
+//        System.out.println("algotmp = " + algotmp);
+//        int loop = 0;
+//        if (algotmp < 33 || algotmp > 126){
+//            for (int i = 0; i < 93; i++){
+//                algo += 1;
+//                loop++;
+//            }
+//        }
+//        System.out.println("algo = " + algo);
+//        System.out.println("loop = " + loop);
 
         /*
          * char Usage examples
@@ -46,47 +60,11 @@ public class Encrypt {
          * Encrypting word
          * Move char in word up by charcode of int:algo
          */
-        //String strtmp = "";
-        //Process algotmp at position 
-
-        //Process word at position wpos
-//        for (int wpos = 0; wpos < w.length(); wpos++) {
-//            System.out.println("wpos is now = " + wpos);
-//            for (int apos = 0; apos < String.valueOf(algotmp).length(); apos++) {
-//                System.out.println("apos is now = " + apos);
-//                strtmp += (char) (w.codePointAt(wpos) + String.valueOf(algotmp).codePointAt(apos));
-//
-//                //strtmp += String.valueOf((char) (w.charAt(i) + String.valueOf(algotmp).codePointAt(x)));
-//                //System.out.println("w.charat " + i + " = " + (char) w.charAt(i));
-//            }
-//            
-//            System.out.println("strtmp = " + strtmp);
-//        }
-            //word += String.valueOf((char) (w.charAt(i) + algotmp));
-
-        //System.out.println("char at i=" + i + " is " + (char) (w.charAt(i) +1));
-        //System.out.println("word is now scrambled and is now = " + word);
-        
-        String str = "";
-        //if word at position 0 = A then shows B
-        //str += (char) (w.charAt(0) + String.valueOf(algotmp).charAt(0));
-        //System.out.println("algotmp 0 = " + String.valueOf(algotmp).charAt(0));
-        //System.out.println("str = " + str);
-        
-        //asdf = 414
-        //turn str to 41441441...
-        //asdfghjkl = 912
-        //turn str to 912912912...
-        //for (int wpos = 0; wpos < w.length(); wpos++){
-           
-        for (int a = 0; a < String.valueOf(algotmp).length(); a++) {
-            str += String.valueOf(algotmp).charAt(a);
-           
+        for (int i = 0; i < w.length(); i++) {
+            word += String.valueOf((char) (w.charAt(i) + algotmp));
+            //System.out.println("char at i=" + i + " is " + (char) (w.charAt(i) +1));
         }
-        //}
-        System.out.println("str = " + str);
-        
-        
+        //System.out.println("word is now scrambled and is now = " + word);
     }
 
     public String returnText() {
